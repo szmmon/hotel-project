976d56d550f5ffda9425392bbff9a14b8f0e1f82
Szymon Ziemianek <szymonziemianek@gmail.com>
2023-11-04 10:35:31 +0100

    fixed dates display for database

--- index.php
+++ index.php
@@ -3,16 +3,54 @@ include("database.php");
 include("index.html");
 
 
-    $reserve_btn = $_POST["reserve-btn"];
-    if(isset($reserve_btn)){
-        $people_count = $_POST["people_count"];
+
+    function date_formating($daterange){
+        $datestart = substr($daterange, 0, strpos($daterange, " "));
+        $dateend = substr($daterange, 13);
+        $datestart = str_replace("/", "-", $datestart);
+        $dateend = str_replace("/", "-", $dateend);
+   
+        $datestart = substr($datestart, 6) . "-" . substr($datestart,0,3) . substr($datestart,3,6);
+        $dateend = substr($dateend, 6) . "-" . substr($dateend,0,3) . substr($dateend,3,6);
+
+
+        $datestart = substr($datestart, 0, 10);
+        $dateend = substr($dateend, 0, 10);
+
+        
+        $dates = array($datestart, $dateend);
+
+        return $dates;
+    }
+
+
+    $reserve_btn_suite = $_POST["reserve-btn-suite"];
+    if(isset($reserve_btn_suite)){
+        $people_count = $_POST["people-count"];
         $reservation_name = $_POST["reservation-name"];
         $daterange = $_POST["daterange"];
+
+        $room = "suite";
         
+        $datestart = date_formating($daterange)[0];
+        $dateend = date_formating($daterange)[1];
+
+
         echo"$people_count <br>";
         echo"$reservation_name <br>";
         echo"$daterange <br>";
+        echo"$datestart<br>";
+        echo"$dateend<br>";
 
+        $sql= "INSERT into bookings (booking_id, name, room, guests_num, date_start, date_end) VALUES (NULL, '$reservation_name', '$room', '$people_count', '$datestart', '$dateend')";
+
+    }
+
+    try{
+        mysqli_query($conn, $sql);
+    }
+    catch(mysqli_sql_exception){
+        echo "Could not reserve";
 
     }
 
--- script.js
+++ script.js
@@ -146,17 +146,18 @@ const datepicker = function () {
   $('input[name="daterange"]').daterangepicker(
     {
       opens: "left",
-      startDate: `${moment().format("l")}`,
+      startDate: `${moment()}`,
       endDate: `${moment().add(12, "months").calendar()}`,
-    },
-    function (start, end, label) {
-      console.log(
-        "A new date selection was made: " +
-          start.format("YYYY-MM-DD") +
-          " to " +
-          end.format("YYYY-MM-DD")
-      )
     }
+
+    // function (start, end, label) {
+    //   console.log(
+    //     "A new date selection was made: " +
+    //       start.format("YYYY-MM-DD") +
+    //       " to " +
+    //       end.format("YYYY-MM-DD")
+    //   )
+    // }
   )
 }
 datepicker()
